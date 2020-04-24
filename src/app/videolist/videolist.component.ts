758fa1d67853807103e9db18dd2c3f462d5bd6e6
vishaljkk <vishal.kk@somaiaya.edu>
2020-04-24 15:56:08 +0530

    please chal bhai

--- src/app/videolist/videolist.component.ts
+++ src/app/videolist/videolist.component.ts
@@ -12,9 +12,9 @@ export class VideolistComponent implements OnInit {
   public SelectVideo = new EventEmitter();
   videos : Video[]=[
     {"_id":"1","title":"Title 1","url":"https://www.w3schools.com/", "description": "98798"},
-    {"_id":"2","title":"Title 2","url":"https://www.youtube.com/watch?v=5U9uP6riAZM&list=RD5U9uP6riAZM&start_radio=1", "description": "768778"},
-    {"_id":"3","title":"Title 3","url":"https://www.youtube.com/watch?v=kqmsZdE962Q&list=RDkqmsZdE962Q&start_radio=1", "description": "767689"},
-    {"_id":"4","title":"Title 4","url":"https://www.youtube.com/watch?v=LWh5u9c6iZQ", "description": "98907"},
+    {"_id":"2","title":"Title 2","url":"https://www.youtube.com/embed/5U9uP6riAZM&list=RD5U9uP6riAZM&start_radio=1", "description": "768778"},
+    {"_id":"3","title":"Title 3","url":"https://www.youtube.com/embed/kqmsZdE962Q&list=RDkqmsZdE962Q&start_radio=1", "description": "767689"},
+    {"_id":"4","title":"Title 4","url":"https://www.youtube.com/embed/LWh5u9c6iZQ", "description": "98907"},
   ];
   constructor() { }
   ngOnInit(): void {
